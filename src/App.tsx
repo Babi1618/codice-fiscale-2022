7e925dedf504721e9c410d5a389c09f12fc44acc
Babi1618 <74872626+Babi1618@users.noreply.github.com>
2022-09-02 13:03:19 +0200

    componente grafica da implementare

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-// import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import CodiceFiscale from './_components/CodiceFiscale';
 
 function App() {
 	return (
 		<div >
-			<CodiceFiscale />
+			{/* DA IMPLEMENTARE */}
+			{/* <CodiceFiscale /> */}
 		</div>
 
 	);
