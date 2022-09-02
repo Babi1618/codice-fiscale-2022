1ce5ef1223a0c5ed8697f00c6855e78c5ded54b9
Babi1618 <74872626+Babi1618@users.noreply.github.com>
2022-09-02 13:36:53 +0200

    install moment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import './App.css';
 
 function App() {
+
+	const persona1 = {
+		nome: "Gino",
+		cognome: "Rossi",
+		sesso: "M",
+		dataNascita: "93/03/12",
+		codiceCatastaleNascita: "A944",
+	}
+
+	const today = new Date()
+	console.log(today)
+
 	return (
 		<div >
 		</div>
