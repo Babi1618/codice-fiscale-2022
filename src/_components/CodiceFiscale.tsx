f75f991d12cc8627db7b130e039c8fc2b975d66f
Babi1618 <74872626+Babi1618@users.noreply.github.com>
2022-09-05 18:45:55 +0200

    base react-hook-form

--- src/_components/CodiceFiscale.tsx
+++ src/_components/CodiceFiscale.tsx
@@ -1,34 +1,41 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const CodiceFiscale = () => {
+	const { handleSubmit } = useForm()
+	const onClickCalcola = (data: any) => {
+		console.log(data)
+	}
 	return (
 		<div className='container'>
 			<h1>Calcolo codice Fiscale</h1>
-			<div className='row'>
-				<div className='col-5'>Cognome
+			<form onSubmit={handleSubmit(onClickCalcola)}>
+				<div className='row'>
+					<div className='col-5'>Cognome
+
+					</div>
+					<div className='col-5'>Nome
+
+					</div>
+					<div className='col-2'>Sesso
+
+					</div>
+				</div>
+				<div className='row'>
+					<div className='col-6'>Luogo Nascita
+
+					</div>
+					<div className='col-6'>Data Nascita
+
+					</div>
+				</div>
+				<div className='row justify-content-center'>
+					<div className='col'>
+						<button>Calcola</button>
+					</div>
 
 				</div>
-				<div className='col-5'>Nome
-
-				</div>
-				<div className='col-2'>Sesso
-
-				</div>
-			</div>
-			<div className='row'>
-				<div className='col-6'>Luogo Nascita
-
-				</div>
-				<div className='col-6'>Data Nascita
-
-				</div>
-			</div>
-			<div className='row justify-content-center'>
-				<div className='col'>
-					<button>Calcola</button>
-				</div>
-
-			</div>
+			</form>
 
 		</div>
 	)
