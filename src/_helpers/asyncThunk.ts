ffff3e3e99d7f9907ba18ce392f09e9fc2387ee8
Babi1618 <74872626+Babi1618@users.noreply.github.com>
2022-09-08 21:36:06 +0200

    ordine

--- src/_helpers/asyncThunk.ts
+++ src/_helpers/asyncThunk.ts
@@ -7,9 +7,7 @@ export const getRegioniAsync: any = createAsyncThunk("cf/regioni",
 		if (response.ok) {
 			const lista_regioni: any = []
 			const regioni = await response.json();
-			// console.log(lista_regioni)
 			regioni.map((regione: string) => {
-				console.log(regione)
 				lista_regioni.push({ "text": regione, "value": regione })
 			})
 			return { lista_regioni }
